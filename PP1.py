e72d6a27214d18bb6cdda422cb79c227822f1e3a
nicsFantasy <vinicius.edwards@gmail.com>
2020-08-18 18:30:52 -0400

    WOW, IT REALLY WORKS

--- PP1.py
+++ PP1.py
@@ -3,6 +3,7 @@ import re
 def match(regex, string):
     p = re.compile(regex)
     return p.search(string)
+
 def spam ():
     print("spam")
     exit()
@@ -11,7 +12,7 @@ def spam ():
 fileName = str(input())
 #fileName = "email.txt"
 
-file = open(fileName[:-1])
+file = open(fileName[:-1], "r")
 content = file.readlines()
 
 regexList = ["-{5}beginmessage-{5}", \
@@ -36,7 +37,6 @@ for j in range(6, len(content)):
     else:
         mensagem += content[j]
         counter += 1
-    #print(content[j])
 if len(content)-6 <= counter:
     spam()
 
@@ -55,38 +55,5 @@ if len(pontuacao) > 15:
 
 print("ham")
 
-# #Checa inicio da mensagem
-# match = re.search(r"-{5}beginmessage-{5}", content[0])
-
-# if match:
-#     print("begin message")
-#     # Checa remetente
-#     match = re.search(r"from: [^@]+@[^@]+\.[^@]+", content[1])
-
-#     if match:
-#         print("EMAIL AAAAAAAA")
-#         # Checa Destinatário
-#         match = re.search(r"to: [^@]+@[^@]+\.[^@]+", content[2])
-
-#         if match:
-#             print("EMAIL BBBBBBB")
-#             # Checa endereço IP
-#             match = re.search(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", content[3])
-        
-#             if match:
-#                 print("IP valido")
-#                 # Checa Formato de data e hora (aceita ano bissexto)
-#                 match = re.search(r"^(19|20)\d\d\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01]) (?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$", content[4])
-
-#                 if match:
-#                     print("data e hora correta")
-
-#                     match = re.search(r"^(-{23})$", content[5])
-
-#                     if match:
-#                         print("Fim do cabeçalho")
-
-#print(content)
-#print(regexList)
 
 file.close()
