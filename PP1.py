612600a985068639a3a7642e877bec0f0cb10960
nicsFantasy <vinicius.edwards@gmail.com>
2020-08-18 11:53:36 -0400

    lista com os regex, e funçao pra diminuir o codigo

--- PP1.py
+++ PP1.py
@@ -1,43 +1,65 @@
 import re
 
+def match(regex, string):
+    p = re.compile(regex)
+    return p.search(string)
+
+
 #fileName = str(input())
 fileName = "email.txt"
 
 file = open(fileName, 'r', encoding="utf8")
 content = file.readlines()
 
-#Checa inicio da mensagem
-match = re.search(r"-{5}beginmessage-{5}", content[0])
+regexList = ["-{5}beginmessage-{5}", \
+    "from: [^@]+@[^@]+\.[^@]+", \
+    "to: [^@]+@[^@]+\.[^@]+", \
+    "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", \
+    "^(19|20)\d\d\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01]) (?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$", \
+    "^(-{23})$"]
 
-if match:
+if match(regexList[0], content[0]):
     print("begin message")
-    # Checa remetente
-    match = re.search(r"from: [^@]+@[^@]+\.[^@]+", content[1])
 
-    if match:
-        print("EMAIL AAAAAAAA")
-        # Checa Destinatário
-        match = re.search(r"to: [^@]+@[^@]+\.[^@]+", content[2])
+# try:
+#     while True:
+#         def
+# except EOFError:
+#     pass
+    
 
-        if match:
-            print("EMAIL BBBBBBB")
-            # Checa endereço IP
-            match = re.search(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", content[3])
+# #Checa inicio da mensagem
+# match = re.search(r"-{5}beginmessage-{5}", content[0])
+
+# if match:
+#     print("begin message")
+#     # Checa remetente
+#     match = re.search(r"from: [^@]+@[^@]+\.[^@]+", content[1])
+
+#     if match:
+#         print("EMAIL AAAAAAAA")
+#         # Checa Destinatário
+#         match = re.search(r"to: [^@]+@[^@]+\.[^@]+", content[2])
+
+#         if match:
+#             print("EMAIL BBBBBBB")
+#             # Checa endereço IP
+#             match = re.search(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", content[3])
         
-            if match:
-                print("IP valido")
-                # Checa Formato de data e hora (aceita ano bissexto)
-                match = re.search(r"^(19|20)\d\d\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01]) (?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$", content[4])
+#             if match:
+#                 print("IP valido")
+#                 # Checa Formato de data e hora (aceita ano bissexto)
+#                 match = re.search(r"^(19|20)\d\d\.(0[1-9]|1[012])\.(0[1-9]|[12][0-9]|3[01]) (?:([01]?\d|2[0-3]):([0-5]?\d):)?([0-5]?\d)$", content[4])
 
-                if match:
-                    print("data e hora correta")
+#                 if match:
+#                     print("data e hora correta")
 
-                    match = re.search(r"^(-{23})$", content[5])
+#                     match = re.search(r"^(-{23})$", content[5])
 
-                    if match:
-                        print("Fim do cabeçalho")
+#                     if match:
+#                         print("Fim do cabeçalho")
 
 print(content)
-
+print(regexList)
 
 file.close()
